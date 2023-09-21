7b8213d7ff5ff8a5de0ba6a95d2bd20433660f21
lucy-xfl <86309711+lucy-xfl@users.noreply.github.com>
2023-09-21 02:20:30 -0700

    Update the model and controllers
    Change into MVC structure
    Import data to the db
    Rewrite CRUD with mongoose

---
+++ start/models/tourModel.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A tour must have a name'],
+    unique: true
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size']
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either: easy, medium, difficult'
+    }
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0']
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
+  },
+  price: {
+    type: Number,
+    require: [true, 'A tour must have a name']
+  },
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function(val) {
+        // this only points to current doc on NEW document creation
+        return val < this.price;
+      },
+      message: 'Discount price ({VALUE}) should be below regular price'
+    }
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false
+  },
+  startDates: [Date]
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
