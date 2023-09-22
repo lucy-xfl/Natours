c8310943ebdac571ebd79bfd837e778b1e1a073f
lucy-xfl <86309711+lucy-xfl@users.noreply.github.com>
2023-09-21 21:56:21 -0700

    Refactoring API Features
    Create a new document to save the API Features for further reuse

--- start/controllers/tourController.js
+++ start/controllers/tourController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -10,47 +11,13 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    //Build query
-    //filtering
-    const queryObj = { ...req.query };
-    const excludeFields = ['page', 'sort', 'limit', 'fields'];
-    excludeFields.forEach(el => delete queryObj[el]);
-
-    //advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    console.log(JSON.parse(queryStr));
-
-    let query = Tour.find(queryObj);
-
-    //2)Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    //3)Field limiting
-    if (req.query.fields) {
-      const fields = req.query.sort.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    //4)Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exists');
-    }
-
     //excute query
-    const tours = await query;
+    const fratures = new APIFeatures(Tour.find(), req.query())
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await fratures.query;
     // query.sort().select().skip().limit()
 
     //send response
