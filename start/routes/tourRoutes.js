e75aa4030d70968bdbac51b873b7171bae616a2e
lucy-xfl <86309711+lucy-xfl@users.noreply.github.com>
2023-09-23 01:02:17 -0700

    Big building!!! Authentication,  Authorization and Security
    Create user model
    Add sign up and login function
    Add reset password function through email
    Add deleting the current User
    Add Updating the Current User
    Data Sanitization
    Preventing parameter Pollution
    Use token-base, limit body payload, storing JWT in HTTP cookies, bcrypt to make the login request slow to make sure the security

--- start/routes/tourRoutes.js
+++ start/routes/tourRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
+
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
@@ -13,13 +15,17 @@ router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/')
-  .get(tourController.getAllTours)
+  .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
 
 router
   .route('/:id')
   .get(tourController.getTour)
   .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour
+  );
 
 module.exports = router;
